072cd1665fa7148794ab936dc8af46783b3b0eee
Joel Taylor <joeltaylor@github.com>
2022-08-25 10:00:14 -0600

    04 emit events

--- app/controllers/time_sinks_controller.rb
+++ app/controllers/time_sinks_controller.rb
@@ -1,6 +1,18 @@
 class TimeSinksController < ApplicationController
   def create
-    flash[:notice] = "The silicon has determined you will wait for #{rand(100)} minutes!"
+    # We're going to simulate a service that sends events based on usage. This is a convenience
+    # for demonstration.
+    time_to_wait = rand(100)
+    flash[:notice] = "The silicon has determined you will wait for #{time_to_wait} minutes!"
+
+    PayToWait.lago_client.events.create(
+      transaction_id: "#{Time.now.to_i}-#{current_user.id}",
+      customer_id: current_user.id,
+      code: "sum_minutes",
+      timestamp: Time.now.to_i,
+      properties: { minutes: time_to_wait}
+    )
+
     redirect_to root_path
   end
 end
