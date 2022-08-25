2c5361cfcd1443b2bcd1476f871eb8a4ac954441
Joel Taylor <joeltaylor@github.com>
2022-08-25 09:10:29 -0600

    Docker workaround

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Bypass to allow for webhook from Lago
+
+  config.hosts.clear
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
